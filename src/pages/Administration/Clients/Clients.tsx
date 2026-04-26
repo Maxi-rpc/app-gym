5ca819c4772eec88c9932c88d32d33d46f6edf0f
Maximiliano <maxirpc2607@gmail.com>
2026-04-26 15:01:55 -0300

    Update Clients.tsx

--- src/pages/Administration/Clients/Clients.tsx
+++ src/pages/Administration/Clients/Clients.tsx
@@ -4,6 +4,9 @@ import PageMeta from "../../../components/common/PageMeta";
 
 import Label from "../../../components/form/Label";
 import Input from "../../../components/form/input/InputField";
+import { useModal } from "../../../hooks/useModal";
+import { Modal } from "../../../components/ui/modal";
+import Button from "../../../components/ui/button/Button";
 
 type Client = {
 	id: number;
@@ -38,8 +41,8 @@ const mockData: Client[] = [
 		status: "Valid",
 		nextPaid: "12-02-2026",
 		updateDate: "12-01-2026",
-    },
-    {
+	},
+	{
 		id: 3,
 		name: "Pepito",
 		lastname: "Pepito",
@@ -47,8 +50,8 @@ const mockData: Client[] = [
 		status: "Valid",
 		nextPaid: "12-02-2026",
 		updateDate: "12-01-2026",
-    },
-    {
+	},
+	{
 		id: 4,
 		name: "Pepa",
 		lastname: "Pepito",
@@ -60,6 +63,14 @@ const mockData: Client[] = [
 ];
 
 export default function Clients() {
+	const { isOpen, openModal, closeModal } = useModal();
+
+	const handleSave = () => {
+		// Handle save logic here
+		console.log("Saving changes...");
+		closeModal();
+	};
+
 	const [sortConfig, setSortConfig] = useState<SortConfig>({
 		key: "id",
 		direction: "asc",
@@ -109,7 +120,7 @@ export default function Clients() {
 				description="Panel de administracion para clientes"
 			/>
 			<PageBreadcrumb pageTitle="Clients" />
-			<div className="rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12">
+			<div className="rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/3 xl:px-10 xl:py-12">
 				<div className="mx-auto w-full text-center mb-8">
 					<h3 className="mb-4 font-semibold text-gray-800 text-theme-xl dark:text-white/90 sm:text-2xl">
 						Listado de clientes
@@ -126,11 +137,31 @@ export default function Clients() {
 						<Label htmlFor="inputTwo">Buscar Cliente</Label>
 						<Input type="text" id="inputTwo" placeholder="nombre o appelido" />
 					</div>
+
 					<button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 sm:py-2 sm:px-6 rounded text-sm sm:text-base whitespace-nowrap">
-						Agregar
-                    </button>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 sm:py-2 sm:px-6 rounded text-sm sm:text-base whitespace-nowrap">
 						Actualizar
+					</button>
+
+					<button
+						onClick={openModal}
+						className="flex items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/3 dark:hover:text-gray-200 lg:inline-flex lg:w-auto"
+					>
+						<svg
+							className="fill-current"
+							width="18"
+							height="18"
+							viewBox="0 0 18 18"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fillRule="evenodd"
+								clipRule="evenodd"
+								d="M15.0911 2.78206C14.2125 1.90338 12.7878 1.90338 11.9092 2.78206L4.57524 10.116C4.26682 10.4244 4.0547 10.8158 3.96468 11.2426L3.31231 14.3352C3.25997 14.5833 3.33653 14.841 3.51583 15.0203C3.69512 15.1996 3.95286 15.2761 4.20096 15.2238L7.29355 14.5714C7.72031 14.4814 8.11172 14.2693 8.42013 13.9609L15.7541 6.62695C16.6327 5.74827 16.6327 4.32365 15.7541 3.44497L15.0911 2.78206ZM12.9698 3.84272C13.2627 3.54982 13.7376 3.54982 14.0305 3.84272L14.6934 4.50563C14.9863 4.79852 14.9863 5.2734 14.6934 5.56629L14.044 6.21573L12.3204 4.49215L12.9698 3.84272ZM11.2597 5.55281L5.6359 11.1766C5.53309 11.2794 5.46238 11.4099 5.43238 11.5522L5.01758 13.5185L6.98394 13.1037C7.1262 13.0737 7.25666 13.003 7.35947 12.9002L12.9833 7.27639L11.2597 5.55281Z"
+								fill=""
+							/>
+						</svg>
+						Agregar
 					</button>
 				</div>
 
@@ -235,6 +266,52 @@ export default function Clients() {
 					</table>
 				</div>
 			</div>
+			{/* Modal */}
+			<Modal isOpen={isOpen} onClose={closeModal} className="max-w-175 m-4">
+				<div className="relative w-full p-4 overflow-y-auto bg-white no-scrollbar rounded-3xl dark:bg-gray-900 lg:p-11">
+					<div className="px-2 pr-14">
+						<h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
+							Edit Address
+						</h4>
+						<p className="mb-6 text-sm text-gray-500 dark:text-gray-400 lg:mb-7">
+							Update your details to keep your profile up-to-date.
+						</p>
+					</div>
+					<form className="flex flex-col">
+						<div className="px-2 overflow-y-auto custom-scrollbar">
+							<div className="grid grid-cols-1 gap-x-6 gap-y-5 lg:grid-cols-2">
+								<div>
+									<Label>Country</Label>
+									<Input type="text" value="United States" />
+								</div>
+
+								<div>
+									<Label>City/State</Label>
+									<Input type="text" value="Arizona, United States." />
+								</div>
+
+								<div>
+									<Label>Postal Code</Label>
+									<Input type="text" value="ERT 2489" />
+								</div>
+
+								<div>
+									<Label>TAX ID</Label>
+									<Input type="text" value="AS4568384" />
+								</div>
+							</div>
+						</div>
+						<div className="flex items-center gap-3 px-2 mt-6 justify-end">
+							<Button size="sm" variant="outline" onClick={closeModal}>
+								Close
+							</Button>
+							<Button size="sm" onClick={handleSave}>
+								Guardar
+							</Button>
+						</div>
+					</form>
+				</div>
+			</Modal>
 		</div>
 	);
 }
