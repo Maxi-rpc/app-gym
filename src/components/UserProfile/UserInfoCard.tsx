4d2e255a7e762d3f6815de44d87a11e1a893d90e
Maximiliano <maxirpc2607@gmail.com>
2026-06-20 23:05:12 -0300

    Update UserInfoCard.tsx

--- src/components/UserProfile/UserInfoCard.tsx
+++ src/components/UserProfile/UserInfoCard.tsx
@@ -20,13 +20,13 @@ export default function UserInfoCard() {
 			<div className="flex flex-col gap-6 lg:flex-row lg:items-start lg:justify-between">
 				<div>
 					<h4 className="text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-6">
-						Personal Information
+						Información personal
 					</h4>
 
 					<div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-7 2xl:gap-x-32">
 						<div>
 							<p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-								First Name
+								Nombre
 							</p>
 							<p className="text-sm font-medium text-gray-800 dark:text-white/90">
 								{user?.name}
@@ -35,7 +35,7 @@ export default function UserInfoCard() {
 
 						<div>
 							<p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-								Last Name
+								Apellido
 							</p>
 							<p className="text-sm font-medium text-gray-800 dark:text-white/90">
 								{user?.lastname}
@@ -44,7 +44,7 @@ export default function UserInfoCard() {
 
 						<div>
 							<p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-								Email address
+								Email
 							</p>
 							<p className="text-sm font-medium text-gray-800 dark:text-white/90">
 								{user?.email}
@@ -53,7 +53,7 @@ export default function UserInfoCard() {
 
 						<div>
 							<p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-								Phone
+								Teléfono
 							</p>
 							<p className="text-sm font-medium text-gray-800 dark:text-white/90">
 								{user?.phone}
@@ -90,7 +90,7 @@ export default function UserInfoCard() {
 							fill=""
 						/>
 					</svg>
-					Edit
+					Editar
 				</button>
 			</div>
 
@@ -98,17 +98,17 @@ export default function UserInfoCard() {
 				<div className="no-scrollbar relative w-full max-w-175 overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11">
 					<div className="px-2 pr-14">
 						<h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
-							Edit Personal Information
+							Editar información personal
 						</h4>
 						<p className="mb-6 text-sm text-gray-500 dark:text-gray-400 lg:mb-7">
-							Update your details to keep your profile up-to-date.
+							Actualiza tus datos para mantener tu perfil al día.
 						</p>
 					</div>
 					<form className="flex flex-col">
 						<div className="custom-scrollbar h-112.5 overflow-y-auto px-2 pb-3">
 							<div>
 								<h5 className="mb-5 text-lg font-medium text-gray-800 dark:text-white/90 lg:mb-6">
-									Social Links
+									Redes Sociales
 								</h5>
 
 								<div className="grid grid-cols-1 gap-x-6 gap-y-5 lg:grid-cols-2">
@@ -141,27 +141,27 @@ export default function UserInfoCard() {
 							</div>
 							<div className="mt-7">
 								<h5 className="mb-5 text-lg font-medium text-gray-800 dark:text-white/90 lg:mb-6">
-									Personal Information
+									Información personal
 								</h5>
 
 								<div className="grid grid-cols-1 gap-x-6 gap-y-5 lg:grid-cols-2">
 									<div className="col-span-2 lg:col-span-1">
-										<Label>First Name</Label>
+										<Label>Nombre</Label>
 										<Input type="text" value={user?.name} />
 									</div>
 
 									<div className="col-span-2 lg:col-span-1">
-										<Label>Last Name</Label>
+										<Label>Apellido</Label>
 										<Input type="text" value={user?.lastname} />
 									</div>
 
 									<div className="col-span-2 lg:col-span-1">
-										<Label>Email Address</Label>
+										<Label>Email</Label>
 										<Input type="text" value={user?.email} />
 									</div>
 
 									<div className="col-span-2 lg:col-span-1">
-										<Label>Phone</Label>
+										<Label>Teléfono</Label>
 										<Input type="text" value={user?.phone} />
 									</div>
 
@@ -174,10 +174,10 @@ export default function UserInfoCard() {
 						</div>
 						<div className="flex items-center gap-3 px-2 mt-6 lg:justify-end">
 							<Button size="sm" variant="outline" onClick={closeModal}>
-								Close
+								Cerrar
 							</Button>
 							<Button size="sm" onClick={handleSave}>
-								Save Changes
+								Guardar cambios
 							</Button>
 						</div>
 					</form>
