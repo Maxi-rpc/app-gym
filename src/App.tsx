75da29709abe93dce2575731a0323a0608fc1902
Maximiliano <maxirpc2607@gmail.com>
2026-06-20 23:48:30 -0300

    se quitan paginas no usadas, se actualiza login

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
-import SignIn from "./pages/AuthPages/SignIn";
-import SignUp from "./pages/AuthPages/SignUp";
-import NotFound from "./pages/OtherPage/NotFound";
-import UserProfiles from "./pages/UserProfiles";
-import Videos from "./pages/UiElements/Videos";
-import Images from "./pages/UiElements/Images";
-import Alerts from "./pages/UiElements/Alerts";
-import Badges from "./pages/UiElements/Badges";
-import Avatars from "./pages/UiElements/Avatars";
-import Buttons from "./pages/UiElements/Buttons";
-import LineChart from "./pages/Charts/LineChart";
-import BarChart from "./pages/Charts/BarChart";
-import Calendar from "./pages/Calendar";
-import BasicTables from "./pages/Tables/BasicTables";
-import FormElements from "./pages/Forms/FormElements";
-import Blank from "./pages/Blank";
-import AppLayout from "./layout/AppLayout";
+
 import { ScrollToTop } from "./components/common/ScrollToTop";
-import Home from "./pages/Dashboard/Home";
 import { AuthProvider } from "./context/AuthContext";
 import { ProtectedRoute } from "./components/common/ProtectedRoute";
+import AppLayout from "./layout/AppLayout";
 
-// pages
+import NotFound from "./pages/OtherPage/NotFound";
+import Home from "./pages/Dashboard/Home";
+
+// auth
+import SignIn from "./pages/AuthPages/SignIn";
+import SignUp from "./pages/AuthPages/SignUp";
+
+// profile
+import UserProfiles from "./pages/UserProfiles";
+
+// admin
 import Dashboards from './pages/Administration/Dashboard/Dashboard';
 import Clients from "./pages/Administration/Clients/Clients";
 import Coachs from "./pages/Administration/Coachs/Coachs";
 
+// operations
 import Payments from './pages/Operations/Payments/Payments';
 
 export default function App() {
@@ -49,7 +44,7 @@ export default function App() {
 						>
 							<Route index path="/" element={<Home />} />
 
-							{/* Others Page */}
+							{/* profile Page */}
 							<Route
 								path="/profile"
 								element={
@@ -58,33 +53,6 @@ export default function App() {
 									</ProtectedRoute>
 								}
 							/>
-							<Route
-								path="/calendar"
-								element={
-									<ProtectedRoute requiredRoles={["admin", "coach"]}>
-										<Calendar />
-									</ProtectedRoute>
-								}
-							/>
-							<Route path="/blank" element={<Blank />} />
-
-							{/* Forms */}
-							<Route path="/form-elements" element={<FormElements />} />
-
-							{/* Tables */}
-							<Route path="/basic-tables" element={<BasicTables />} />
-
-							{/* Ui Elements */}
-							<Route path="/alerts" element={<Alerts />} />
-							<Route path="/avatars" element={<Avatars />} />
-							<Route path="/badge" element={<Badges />} />
-							<Route path="/buttons" element={<Buttons />} />
-							<Route path="/images" element={<Images />} />
-							<Route path="/videos" element={<Videos />} />
-
-							{/* Charts */}
-							<Route path="/line-chart" element={<LineChart />} />
-							<Route path="/bar-chart" element={<BarChart />} />
 
 							{/* Admin Clients Page */}
 							<Route path="/administration">
