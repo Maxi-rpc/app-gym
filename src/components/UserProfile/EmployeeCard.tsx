80619870ab0ae4fe534d2de9dacb4b2ffc4ec5c6
Maximiliano <maxirpc2607@gmail.com>
2026-07-24 01:20:43 -0300

    Update EmployeeCard.tsx

--- src/components/UserProfile/EmployeeCard.tsx
+++ src/components/UserProfile/EmployeeCard.tsx
@@ -25,6 +25,12 @@ export default function EmployeeCard() {
 	const [error, setError] = useState("");
 	const [message, setMessage] = useState("");
 
+	const handleCloseModal = () => {
+		setError("");
+		setMessage("");
+		closeModal();
+	};
+
 	const handleSave = async () => {
 		setError("");
 		// Validación básica
@@ -147,7 +153,11 @@ export default function EmployeeCard() {
 				</div>
 			</div>
 			{/* modal */}
-			<Modal isOpen={isOpen} onClose={closeModal} className="max-w-175 m-4">
+			<Modal
+				isOpen={isOpen}
+				onClose={handleCloseModal}
+				className="max-w-175 m-4"
+			>
 				<div className="relative w-full p-4 overflow-y-auto bg-white no-scrollbar rounded-3xl dark:bg-gray-900 lg:p-11">
 					<div className="px-2 pr-14">
 						<h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
@@ -210,7 +220,7 @@ export default function EmployeeCard() {
 							</div>
 						</div>
 						<div className="flex items-center gap-3 px-2 mt-6 lg:justify-end">
-							<Button size="sm" variant="outline" onClick={closeModal}>
+							<Button size="sm" variant="outline" onClick={handleCloseModal}>
 								Cerrar
 							</Button>
 							<Button size="sm" onClick={handleSave}>
