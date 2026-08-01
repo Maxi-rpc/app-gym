2d9aedf5bb7499901e05937611f6247948a2596d
Maximiliano <maxirpc2607@gmail.com>
2026-08-01 18:53:11 -0300

    update

--- src/pages/Coachs/FormDelete.tsx
+++ src/pages/Coachs/FormDelete.tsx
@@ -5,33 +5,9 @@ import Input from "../../components/form/input/InputField";
 import Button from "../../components/ui/button/Button";
 import Alert from "../../components/ui/alert/Alert";
 import { Feedback } from "../../components/ui/alert/types/AlertFeedback";
+import IconSpinner from "../../components/ui/button/IconSpinner";
 
 import { employeeService } from "../../service/employee.service";
-
-const IconSpinner = () => {
-	return (
-		<svg
-			className="size-5 animate-spin text-white"
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-		>
-			<circle
-				className="opacity-25"
-				cx="12"
-				cy="12"
-				r="10"
-				stroke="currentColor"
-				stroke-width="4"
-			></circle>
-			<path
-				className="opacity-75"
-				fill="currentColor"
-				d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-			></path>
-		</svg>
-	);
-};
 
 type Props = {
 	onSubmit?: () => void;
