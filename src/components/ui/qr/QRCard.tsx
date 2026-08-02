8d1b4539544c8ff9370153a0a85b859b82d1c619
Maximiliano <maxirpc2607@gmail.com>
2026-08-02 12:23:12 -0300

    update import

--- src/components/ui/qr/QRCard.tsx
+++ src/components/ui/qr/QRCard.tsx
@@ -4,7 +4,7 @@ type Props = {
 	value?: string | "";
 };
 
-export default function QrCard({ value }: Props) {
+export default function QRCard({ value }: Props) {
 	return (
 		<div className="p-2 mx-auto sm:mx-0 border-gray-20 overflow-hidden rounded-2xl border dark:border-gray-800">
 			<QRCode className="size-34" value={value || "no data"} />
